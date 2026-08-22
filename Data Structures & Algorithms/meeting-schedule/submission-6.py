fc67c5c0f395a4f7e6f7f41c521508937995eab2
Jack Li <38816464+lijaack@users.noreply.github.com>
2026-08-23 04:44:54 +0800

    Add: meeting-schedule - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule/submission-6.py
@@ -0,0 +1,22 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+
+        # Sort meetings by their start time
+        intervals.sort(key=lambda x: x.start)
+
+        for i in range(1, len(intervals)):
+
+            # If the current meeting starts before the previous
+            # meeting ends, the two meetings overlap.
+            if intervals[i].start < intervals[i - 1].end:
+                return False
+
+        return True
