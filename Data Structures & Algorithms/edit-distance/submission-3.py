beca1c25c4408d31148259e71904844cbb62030f
Jack Li <38816464+lijaack@users.noreply.github.com>
2026-09-18 03:11:31 +0800

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = {}
+
+        def dfs(i,j):
+            if i == len(word1) and j == len(word2) :
+                return 0
+            if i < len(word1) and j == len(word2):
+                return len(word1)-i
+            if i == len(word1) and j < len(word2):
+                return len(word2)-j
+
+            if (i,j) in dp:
+                return dp[(i,j)]
+            curr = 0
+            if word1[i] == word2[j]:
+               curr =  dfs(i+1,j+1)
+            else: 
+              curr = 1 + min(dfs(i+1,j), dfs(i+1,j+1), dfs(i,j+1))
+            dp[(i,j)] = curr
+
+            return dp[(i,j)]
+
+        return dfs(0,0)
+
+
