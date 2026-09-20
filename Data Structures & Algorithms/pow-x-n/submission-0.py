e35702366c5accf82d4f5594baf030bff8cea8fd
Jack Li <38816464+lijaack@users.noreply.github.com>
2026-09-20 18:12:55 +0800

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        total = 0
+        total = x ** n
+        return total
+        # for _ in range(n):
+            
+        #     if n < 0 :
+        #         total = total ** 1/x
+        #     total = total ** x
